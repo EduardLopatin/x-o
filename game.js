5544c5b0bba7658b746bdd15e24400ac87da7e95
EduardLopatin <blackbars48@gmail.com>
2016-10-15 19:34:56 +0300

    game mechanics works!

--- game.js
+++ game.js
@@ -1,35 +1,100 @@
 var game = new Game();
 
+game.createField();
+
 function Game() {
-    createField();
-}
+    this.log = [];
+    this.PlayerTurn = 1;
+    this.status = 0;
+    this.createField = function() {
 
-function createField() {
-    document.body.style.textAlign = 'center';
-    document.body.style.marginTop = '100px';
-    createElement('h1','x/o game');
-    createElement('br','');
+        document.body.style.textAlign = 'center';
+        document.body.style.marginTop = '100px';
+        createElement('h1','x/o game');
+        createElement('br');
 
-    for ( var i = 1; i <= 9; i++ ){
+        for ( var i = 0; i <= 8; i++ ){
 
-        if ( i == 4 || i == 7 ) {
-            document.write('<br>')
+            if ( i == 3 || i == 6 ) {
+                createElement('br');
+            }
+            var element = createElement('div', '&nbsp;', i);
+            getStyle(element);
         }
-        var element = createElement('div', '&nbsp;');
-        getStyle(element);
+        function createElement(tag, text, i) {
+
+            var element = document.createElement(tag);
+            element.setAttribute('id', i);
+            element.innerHTML = text;
+            element.onclick = action;
+            document.body.appendChild(element);
+            return element;
+        }
+        function getStyle(block) {
+            block.style.width = '100px';
+            block.style.height = '100px';
+            block.style.border = 'solid';
+            block.style.borderColor = 'gray';
+            block.style.display = 'inline-block';
+            block.style.fontSize = '80px';
+        }
+    };  
+}
+function action() {
+    if(this.nodeName == 'DIV'){
+        var turn = step(this);
+        this.innerHTML = turn;
+        var toLog = {
+            id: this.id,
+            value: turn
+        };
+        game.log.push(toLog);
+        console.log(game);
+        game.status++;
+        checkIt()
+    }
+}
+function step(element) {
+    if (game.PlayerTurn == 1){
+        game.PlayerTurn = 2;
+        element.onclick = null;
+        return 'x'
+    }
+    game.PlayerTurn = 1;
+    element.onclick = null;
+    return 'o'
+}
+function checkIt() {
+    if(
+        (document.getElementById('0').innerHTML == 'x' && document.getElementById('1').innerHTML == 'x' && document.getElementById('2').innerHTML == 'x')||
+        (document.getElementById('3').innerHTML == 'x' && document.getElementById('4').innerHTML == 'x' && document.getElementById('5').innerHTML == 'x')||
+        (document.getElementById('6').innerHTML == 'x' && document.getElementById('7').innerHTML == 'x' && document.getElementById('8').innerHTML == 'x')||
+        (document.getElementById('0').innerHTML == 'x' && document.getElementById('4').innerHTML == 'x' && document.getElementById('8').innerHTML == 'x')||
+        (document.getElementById('2').innerHTML == 'x' && document.getElementById('4').innerHTML == 'x' && document.getElementById('6').innerHTML == 'x')||
+        (document.getElementById('0').innerHTML == 'x' && document.getElementById('3').innerHTML == 'x' && document.getElementById('6').innerHTML == 'x')||
+        (document.getElementById('1').innerHTML == 'x' && document.getElementById('4').innerHTML == 'x' && document.getElementById('7').innerHTML == 'x')||
+        (document.getElementById('2').innerHTML == 'x' && document.getElementById('5').innerHTML == 'x' && document.getElementById('8').innerHTML == 'x')
+    ){
+        console.log('x win');
+    }if(
+        (document.getElementById('0').innerHTML == 'o' && document.getElementById('1').innerHTML == 'o' && document.getElementById('2').innerHTML == 'o')||
+        (document.getElementById('3').innerHTML == 'o' && document.getElementById('4').innerHTML == 'o' && document.getElementById('5').innerHTML == 'o')||
+        (document.getElementById('6').innerHTML == 'o' && document.getElementById('7').innerHTML == 'o' && document.getElementById('8').innerHTML == 'o')||
+        (document.getElementById('0').innerHTML == 'o' && document.getElementById('4').innerHTML == 'o' && document.getElementById('8').innerHTML == 'o')||
+        (document.getElementById('2').innerHTML == 'o' && document.getElementById('4').innerHTML == 'o' && document.getElementById('6').innerHTML == 'o')||
+        (document.getElementById('0').innerHTML == 'o' && document.getElementById('3').innerHTML == 'o' && document.getElementById('6').innerHTML == 'x')||
+        (document.getElementById('1').innerHTML == 'o' && document.getElementById('4').innerHTML == 'o' && document.getElementById('7').innerHTML == 'x')||
+        (document.getElementById('2').innerHTML == 'o' && document.getElementById('5').innerHTML == 'o' && document.getElementById('8').innerHTML == 'x')
+    ){
+        console.log('0 win');
+    }
+    if(game.status == 9){
+        console.log('nichia');
     }
 }
 
-function createElement(tag, text) {
-    var element = document.createElement(tag);
-    element.innerHTML = text;
-    document.body.appendChild(element);
-    return element;
-}
-function getStyle(block) {
-    block.style.width = '100px';
-    block.style.height = '100px';
-    block.style.border = 'solid';
-    block.style.borderColor = 'gray';
-    block.style.display = 'inline-block';
-}
+
+
+
+
+
