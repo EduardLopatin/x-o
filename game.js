5ca6e4e0c6ddb472e270e94c55e8c3696afd56cd
EduardLopatin <blackbars48@gmail.com>
2016-10-18 17:06:28 +0300

    vertical check work

--- game.js
+++ game.js
@@ -4,37 +4,37 @@ console.log(game);
 
 function Game() {
     var vm = this;
-    function createGameOptions() {
-        vm.options = [];
+
+    this.createGameOptions = function() {
+        vm.options = {};
+        vm.result = '';
+        vm.options.startScreen = {};
         vm.options.stepCounter = 0;
         vm.options.restartButton = {};
         vm.options.field = [];
         vm.options.turnStatusInfo = {};
         vm.options.container = {};
-    }
-    createGameOptions();
-    this.startScreenOptions = {};
+    };
+    this.createGameOptions();
     this.changeTurnStatusInfo = function () {
-    this.options.turnStatusInfo.innerHTML = 'Player ' + vm.options.playerStep + ' turn'
+        this.options.turnStatusInfo.innerHTML = 'Player ' + vm.options.playerStep + ' turn'
 
-}
+    }
     this.restart = function () {
         document.body.innerHTML = '';
-        vm.startScreenOptions = {};
-        createGameOptions();
-        vm.startScreenCreator();
+        vm.createGameOptions();
+        vm.createStartScreen();
     };
-    this.startGame = function() {
+    this.start = function() {
         vm.options.playerStep = 1;
         var target = document.getElementById("numberOfCellsId");
-        vm.startScreenOptions.selectedCells =  target.options[target.selectedIndex].value;
-        console.log(vm.startScreenOptions.selectedCells);
+        vm.options.startScreen.selectedCells =  target.options[target.selectedIndex].value;
         document.body.innerHTML = '';
         vm.fieldCreate();
         console.log(game);
     };
     this.fieldCreate = function () {
-        var Cells = this.startScreenOptions.selectedCells;
+        var Cells = this.options.startScreen.selectedCells;
         this.options.container = this.elementFactory(document.body,'div');
 
         this.options.container.style.width = Cells * 80 + 'px';
@@ -62,34 +62,33 @@ function Game() {
         this.options.restartButton.onclick = this.restart;
 
         this.options.turnStatusInfo = this.elementFactory(document.body,'h3');
-        this.options.turnStatusInfo.id = 'turnStatusInfo';
         this.options.turnStatusInfo.innerHTML = 'Player ' + vm.options.playerStep + ' turn'
     };
-    
     this.elementFactory = function(parent,tag) {
         var element = document.createElement(tag);
         parent.appendChild(element);
         return element;
     };
-    this.startScreenCreator = function(){
+    this.createStartScreen = function(){
         document.body.style.textAlign = 'center';
         document.body.style.marginTop = '80px';
-        this.startScreenOptions.topic = this.elementFactory(document.body,'h1');
-        this.startScreenOptions.topic.innerHTML = 'Select size of field';
-        this.startScreenOptions.numberOfCells  = document.createElement('select');
-        this.startScreenOptions.numberOfCells.style.marginRight = '10px';
-        this.startScreenOptions.numberOfCells.id = 'numberOfCellsId';
+        this.options.startScreen.topic = this.elementFactory(document.body,'h1');
+        this.options.startScreen.topic.innerHTML = 'Select size of field';
+        this.options.startScreen.numberOfCells  = document.createElement('select');
+        this.options.startScreen.numberOfCells.style.marginRight = '10px';
+        this.options.startScreen.numberOfCells.id = 'numberOfCellsId';
         for( var i = 3; i <= 10; i++){
-            var optionElement = this.elementFactory(this.startScreenOptions.numberOfCells,'option');
+            var optionElement = this.elementFactory(this.options.startScreen.numberOfCells,'option');
             optionElement.value = i;
             optionElement.text = i + 'x' + i;
         }
-        document.body.appendChild(this.startScreenOptions.numberOfCells);
+        document.body.appendChild(this.options.startScreen.numberOfCells);
         var startButton = this.elementFactory(document.body,'button');
         startButton.innerHTML = 'Start game';
-        startButton.onclick = this.startGame;
+        startButton.onclick = this.start;
     };
     this.setStyleForBlock = function (block, cells) {
+
         block.style.width = cells* 69 / cells + 'px';
         block.style.height = cells* 69 / cells + 'px';
         block.style.border = 'solid';
@@ -97,57 +96,87 @@ function Game() {
         block.style.display = 'inline-block';
         block.style.fontSize = cells* 50 / cells + 'px';
     };
-
-    this.startScreenCreator();
-    this.checkXorY = function (xOrY) {
-        var xOrYturn = 0;
+    this.createStartScreen();
+    this.checkHorizontal = function (palyer) {
+        var count = 0;
         for(var i = 0; i <= vm.options.field.length - 1; i++){
             vm.options.field[i].forEach(function (item,i,arr) {
-                if(item.value == xOrY){
-                    xOrYturn++
+                if(item.value == palyer){
+                    count++
                 }
-            })
-            if(xOrYturn == vm.startScreenOptions.selectedCells){
-                return console.log(xOrY + ' won!')
+            });
+            if(count == vm.options.startScreen.selectedCells){
+                console.log(palyer + ' won!');
+                vm.result = palyer;
+                break;
             }
-            xOrYturn = 0;
+            count = 0;
         }
 
     };
-    this.checkGame = function () {
-if(this.options.stepCounter >= this.startScreenOptions.selectedCells){
-    this.checkXorY('x');
-    this.checkXorY('o');
-}
-
+    this.checkDiagonal = function (palyer) {
+        var count = 0;
+        for(var i = 0;i<=vm.options.field.length - 1; i++){
+            console.log(vm.options.field[i][i].position);
+        }
+    };
+    this.checkVertical = function (palyer) {
+        var count = 0;
+          for(var i = 0; i <=vm.options.startScreen.selectedCells - 1; i++){
+              vm.options.field.forEach(function (item) {
+                  if(item[i].value == palyer){
+                      count++
+                  }
+              });
+              if(count == vm.options.startScreen.selectedCells){
+                  console.log(palyer + ' won!');
+                  vm.result = palyer;
+                  break;
+              }
+                count = 0;
+          }
 
 
     };
 
-    this.step = function () {
-         if(this.options.playerStep == 1){
-             this.options.playerStep = 2;
-             return 'x';
-         }else {
-             this.options.playerStep = 1;
-             return 'o';
-         }
+    this.showResult = function (winner) {
+        alert(winner+"-Player WON!");
+        this.restart();
+    };
+    this.checkGame = function () {
+        if(this.options.stepCounter >= this.options.startScreen.selectedCells){
+            this.checkHorizontal('x');
+            this.checkHorizontal('o');
+            this.checkVertical('x');
+            this.checkVertical('o');
+        }
+        if(this.result == 'x'|| this.result == 'o'){
+            this.showResult(vm.result);
+        }
+
     };
     this.action = function() {
         if(this.nodeName == 'DIV'){
-            this.innerHTML = vm.step();
+            this.innerHTML = step();
             vm.options.stepCounter++;
             vm.changeTurnStatusInfo();
             var blockPositionX = this.id[0];
             var blockPositionY = this.id[1];
             vm.options.field[blockPositionX][blockPositionY].value = this.innerHTML;
-            vm.checkGame();
             this.onclick = null;
+            vm.checkGame();
         }
+        function step() {
+            if(vm.options.playerStep == 1){
+                vm.options.playerStep = 2;
+                return 'x';
+            }else {
+                vm.options.playerStep = 1;
+                return 'o';
+            }
+        };
 
     }
-
-
 }
 
 
